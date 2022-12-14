6f47dce1280dfbb0a5eb1e123cfc98d53fc82cd8
IlyaLevchenko04 <nneo2086@gmail.com>
2022-12-14 22:09:10 +0200

    rehw

--- js/task-06.js
+++ js/task-06.js
@@ -1,8 +1,8 @@
-const input = document.querySelector('#validation-input');
+const input = document.querySelector('input[data-length = "6"]');
 
 const symbols = (evt) => {
     let number = evt.currentTarget.value;
-        if (number.length < 6){
+        if (number.length < Number(input.dataset.length)){
             input.classList.add('invalid');
             input.classList.remove('valid');
         }else{
